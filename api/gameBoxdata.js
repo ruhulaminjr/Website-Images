617165f89448e9981512521edbae611677a8821f
ruhulaminjr <ruhultuber@gmail.com>
2021-12-25 21:08:50 +0600

    photos added

---
+++ api/gameBoxdata.js
@@ -0,0 +1,5 @@
+const sliderData = {
+  id: 1,
+  name: "Cyberpunk 2077",
+  desc: "Cyberpunk 2077 is an open-world, action-adventure story set in Night City, a megalopolis obsessed with power, glamour and body modification.",
+};
